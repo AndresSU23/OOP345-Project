43907e12372c90b2a6fe806d7aa2ff202b569497
David Andres Sanchez Umbarila <sanchezdavid481@gmail.com>
2024-03-23 21:56:24 -0400

    MS2 Submit for Testing

---
+++ MS2/Station.h
@@ -0,0 +1,45 @@
+// Name: David Andres Sanchez Umbarila
+// Seneca Student ID: 140273228
+// Seneca email: dasanchez-umbarila@myseneca.ca
+// Date of completion: 18/03/2024
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#pragma once
+#ifndef SENECA_STATION_H
+#define SENECA_STATION_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+namespace seneca {
+
+    class Station {
+        int m_id;
+        std::string m_itemName;
+        std::string m_description;
+        size_t m_serialNumber;
+        size_t m_quantity;
+
+        static size_t m_widthField;
+        static int id_generator;
+
+    public:
+        Station(const std::string& record);
+
+        const std::string& getItemName() const;
+
+        size_t getNextSerialNumber();
+
+        size_t getQuantity() const;
+
+        void updateQuantity();
+
+        void display(std::ostream& os, bool full) const;
+    };
+
+}
+
+#endif // SENECA_STATION_H
---
+++ MS2/Utilities.h
@@ -0,0 +1,32 @@
+// Name: David Andres Sanchez Umbarila
+// Seneca Student ID: 140273228
+// Seneca email: dasanchez-umbarila@myseneca.ca
+// Date of completion: 18/03/2024
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#pragma once
+#ifndef SENECA_UTILITIES_H
+#define SENECA_UTILITIES_H
+
+#include <string>
+
+namespace seneca {
+	class Utilities {
+		size_t m_widthField{ 1 };
+		static char m_delimeter;
+
+		std::string trim(std::string str) const;
+
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	};
+}
+
+#endif // !SENECA_UTILITIES_H
+
